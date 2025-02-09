c7592cde3d08e0ae95fc3981ac9f306ce951ac9c
yadavaryan17 <aryanyad174@gmail.com>
2025-02-09 10:42:13 +0530

    add array and some basic programs

---
+++ Array/FindLarge.java
@@ -0,0 +1,27 @@
+public class FindLarge {
+
+    public static void FindLargestNo(int num[]){
+        int large = 0;
+        for(int i = 0; i < num.length - 1; i++){
+            if(num[large] < num[i + 1]){
+                large = i + 1;
+            }
+        }
+        System.out.println("Largest no. is "+num[large]+" and index is "+large);
+    }
+    public static void FindLargest_No(int num[]){
+        int large = Integer.MIN_VALUE;
+        for(int i = 0; i < num.length ; i++){
+            if(large < num[i]){
+                large = num[i];
+            }
+        }
+        System.out.println("Largest no. is "+large);
+    }
+
+    public static void main(String str[]){
+        int num[] = {3, 5, 2, 6 ,9 , 1 ,8};
+        FindLargest_No(num);
+    }
+    
+}
---
+++ Array/LinearSear.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LinearSear {
+
+    public static void LinearSearch(int num[] , int key){
+        int j = 0;
+        for(int i = 0; i < num.length; i++){
+            if(num[i] == key){
+                j = i;
+                break;
+            }
+        }
+        System.out.println("you are searching for "+ num[j]+" is in "+ j +" index");
+    }
+
+    public static void main(String str[]){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {3 , 5 , 2 , 9 , 7 , 9};
+        System.out.print("Enter number you searching for : ");
+        int key = sc.nextInt();
+        LinearSearch(arr, key);
+    }
+    
+}
